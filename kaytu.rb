c72420705de8b52c13acc9ee413ed48875b9b684
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-07-03 13:07:31 +0400

    feat: Adds kaytu cli formulae

---
+++ kaytu.rb
@@ -0,0 +1,16 @@
+class Kaytu < Formula
+  desc "CLI application for Kaytu"
+  homepage "https://github.com/kaytu-io/cli-program"
+  url "https://github.com/kaytu-io/cli-program/releases/download/v1.3.13/ktucli-macos-amd64"
+  sha256 "89f4ef14396b9dd75484281ebfac825dc39db335119df09391db9d9919cef11a"
+  version "1.0.0"
+  license "MIT"
+
+  def install
+    bin.install "kaytu"
+  end
+
+  test do
+    system "#{bin}/kaytu", "--version"
+  end
+end
