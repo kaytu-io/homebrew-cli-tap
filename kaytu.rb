f20ef672e3ac94240e136d8bf6fb6fdb9f8af1fc
Kaytu Developer <dev@kaytu.io>
2023-12-18 06:55:23 +0000

    update cli version

--- kaytu.rb
+++ kaytu.rb
@@ -1,36 +1,36 @@
 class Kaytu < Formula
   desc "CLI application for Kaytu"
   homepage "https://github.com/kaytu-io/cli-program"
-  version "1.14.26"
+  version "1.14.27"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kaytu-io/cli-program/releases/download/v1.14.26/ktucli-macos-amd64"
-    sha256 "b348f08b0c5fa7c1eac0fb666345ae424f026bfa2da3e2988a44dc0fe4c2a6ba"
+    url "https://github.com/kaytu-io/cli-program/releases/download/v1.14.27/ktucli-macos-amd64"
+    sha256 "4cb55212dc565e68f05e3287f73a3fb7f38105144b0a3f12d31d9648a78a1c0b"
     def install
       bin.install "ktucli-macos-amd64" => "kaytu"
     end
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/kaytu-io/cli-program/releases/download/v1.14.26/ktucli-macos-arm64"
-    sha256 "59bbd57efb28d4ace42f556af11ce05a30717333f11f0501cb0d0534e2f2344f"
+    url "https://github.com/kaytu-io/cli-program/releases/download/v1.14.27/ktucli-macos-arm64"
+    sha256 "43b5746eafdd24ac824cfdeb44a3779a9c891a7ab6fc1ffa89734bf95b45fae9"
     def install
       bin.install "ktucli-macos-arm64" => "kaytu"
     end
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/kaytu-io/cli-program/releases/download/v1.14.26/ktucli-linux-arm64"
-    sha256 "01f0aedc7bc25cf669e7e0e8219e103b86488f3cda8c94c09e02cd8dcb32de4d"
+    url "https://github.com/kaytu-io/cli-program/releases/download/v1.14.27/ktucli-linux-arm64"
+    sha256 "574a698e5f88b1cbb8aa9c365d77d6351238a72ebdd7834068e61d8865e18c1c"
     def install
       bin.install "ktucli-linux-arm64" => "kaytu"
     end
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kaytu-io/cli-program/releases/download/v1.14.26/ktucli-linux-amd64"
-    sha256 "ba270faffb97b66b573fcf08e86c93f8005ab1bdbd2a38d76d174cad6335e843"
+    url "https://github.com/kaytu-io/cli-program/releases/download/v1.14.27/ktucli-linux-amd64"
+    sha256 "de1836e0b025c244fc20efa66c25f976e69a8333a87ee2192477ddd1ea6a5783"
     def install
       bin.install "ktucli-linux-amd64" => "kaytu"
     end
