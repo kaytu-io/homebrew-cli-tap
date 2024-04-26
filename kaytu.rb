159f889b07d31f73183e180b15b5714aa887761c
Kaytu Developer <dev@kaytu.io>
2024-04-26 20:51:35 +0000

    update cli version

--- kaytu.rb
+++ kaytu.rb
@@ -1,27 +1,27 @@
 class Kaytu < Formula
   desc "CLI application for Kaytu"
   homepage "https://github.com/kaytu-io/cli-program"
-  version "0.3.30"
+  version "0.3.31"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.30/kaytu_0.3.30_darwin_amd64.tar.gz"
-    sha256 "457c08cc201e0f8589d9a1933ccb3f7bd033bc85b259c32e1d93fad621df1a8b"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.31/kaytu_0.3.31_darwin_amd64.tar.gz"
+    sha256 "4b51d95b7fcd1e7d459a1a7f2dbff086981805e64b220f436e682fdad29754f3"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.30/kaytu_0.3.30_darwin_arm64.tar.gz"
-    sha256 "5a29f2b600f063c2dd973849fb05feb1dc8c52911d61b52650bbb1d74943c307"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.31/kaytu_0.3.31_darwin_arm64.tar.gz"
+    sha256 "6d1870e404df5424a287f47ee193fee71379d991005534a20de8c8fa546ac57b"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.30/kaytu_0.3.30_linux_arm64.tar.gz"
-    sha256 "d8d193c7eadf8d0059b006b34414b56de728a06f270c6bc911613fd01ba2edb2"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.31/kaytu_0.3.31_linux_arm64.tar.gz"
+    sha256 "7a849fd89463839bf1c7147ab38f30933969da503f6fdd0060f149dad9f6100c"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.30/kaytu_0.3.30_linux_amd64.tar.gz"
-    sha256 "8763680aa2b806752066e19fa0dbd6537a156d29b427a383bc0097274102ed71"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.31/kaytu_0.3.31_linux_amd64.tar.gz"
+    sha256 "f5de2da5165eac8cb09d0adf7d339bf3b8ae3c15002e04b79e5a70853cc9cfd6"
   end
 
 
