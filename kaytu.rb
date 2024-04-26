750a72760c1c8233466b4b61da08f76326baf241
Kaytu Developer <dev@kaytu.io>
2024-04-26 20:11:21 +0000

    update cli version

--- kaytu.rb
+++ kaytu.rb
@@ -1,38 +1,42 @@
 class Kaytu < Formula
   desc "CLI application for Kaytu"
   homepage "https://github.com/kaytu-io/cli-program"
-  version "0.3.27"
+  version "0.3.28"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.27/kaytu-darwin-amd64"
-    sha256 "82b7f40752946007e99f2cbd332f8742b1a62e45c04662041171d6dfdafa597b"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.28/kaytu_0.3.28_darwin_amd64.tar.gz"
+    sha256 "af26f40cf08f2b13fdaa368aa033a639dff7c9f7a382982cf32d13ecc8596b3f"
     def install
-      bin.install "ktucli-macos-amd64" => "kaytu"
+      system "tar", "-xf", "kaytu_0.3.28_darwin_amd64.tar.gz"
+      bin.install "kaytu_0.3.28_darwin_amd64" => "kaytu"
     end
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.27/kaytu-darwin-arm64"
-    sha256 "6eab6a8d4ba1ae9e15b70ed09543debc4dbc59d7c42957afb019a7b89a22d9f9"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.28/kaytu_0.3.28_darwin_arm64.tar.gz"
+    sha256 "ec91937a866e783acb76f3fce3b450cff1e1a25600df90feb9340743e60a73a8"
     def install
-      bin.install "ktucli-macos-arm64" => "kaytu"
+      system "tar", "-xf", "kaytu_0.3.28_darwin_arm64.tar.gz"
+      bin.install "kaytu_0.3.28_darwin_arm64" => "kaytu"
     end
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.27/kaytu-linux-arm64"
-    sha256 "fa6682a8ec820eca584bb6e0e7a30d93a42a6cedbab8f002bee759e56eef9c71"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.28/kaytu_0.3.28_linux_arm64.tar.gz"
+    sha256 "3f629bfa76c44bb5737ea970c3729353d09411399734fcd94da74a3fb4237c7d"
     def install
-      bin.install "ktucli-linux-arm64" => "kaytu"
+      system "tar", "-xf", "kaytu_0.3.28_linux_arm64.tar.gz"
+      bin.install "kaytu_0.3.28_linux_arm64" => "kaytu"
     end
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.27/kaytu-linux-amd64"
-    sha256 "d3d43d44109bcc99699c90641e53c41d2a50aadd7f9618566e2ea80a5a7304d6"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.28/kaytu_0.3.28_linux_amd64.tar.gz"
+    sha256 "5a5ee5ab8c5f3c53cd0be92ad1e48793d99fb020d7a44350ff43e70c1d01aa99"
     def install
-      bin.install "ktucli-linux-amd64" => "kaytu"
+      system "tar", "-xf", "kaytu_0.3.28_linux_amd64.tar.gz"
+      bin.install "kaytu_0.3.28_linux_amd64" => "kaytu"
     end
   end
 
