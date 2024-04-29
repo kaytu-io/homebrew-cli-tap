82fd0215c394042c6a3630dd2dd56d404b6a484f
Kaytu Developer <dev@kaytu.io>
2024-04-29 12:48:51 +0000

    update cli version

--- kaytu.rb
+++ kaytu.rb
@@ -1,36 +1,36 @@
 class Kaytu < Formula
   desc "CLI application for Kaytu"
   homepage "https://github.com/kaytu-io/cli-program"
-  version "0.3.53"
+  version "0.3.54"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.53/kaytu_0.3.53_darwin_amd64.tar.gz"
-    sha256 "9d78fb87594ae81ed1bd1b19b73be8c61210be297b0a6cc7967d5fe4e22ba9a6"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.54/kaytu_0.3.54_darwin_amd64.tar.gz"
+    sha256 "67fd0661e31e43622830624d39d7018e8c93bcb028d5ff82bfc8c7f26a4dea14"
     def install
       bin.install "kaytu" => "kaytu"
     end
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.53/kaytu_0.3.53_darwin_arm64.tar.gz"
-    sha256 "bb3165616eb8e62177ff39c0a99c9b894db9bef61579ebd85c24ae2ca4f1babc"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.54/kaytu_0.3.54_darwin_arm64.tar.gz"
+    sha256 "8e9fa5c16a0ae0ebda11ef45252f5154013b8a5daf152783fddb2a26edc5243b"
     def install
       bin.install "kaytu" => "kaytu"
     end
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.53/kaytu_0.3.53_linux_arm64.tar.gz"
-    sha256 "ea8e3ec748edb1acf66abce79fb21b7133e4380d1a54d827dd4049b685f6a185"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.54/kaytu_0.3.54_linux_arm64.tar.gz"
+    sha256 "4c0464654c85d27c279988609325e7f78ad91665fc8c0078aa2bcf8b4b858b82"
     def install
       bin.install "kaytu" => "kaytu"
     end
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.53/kaytu_0.3.53_linux_amd64.tar.gz"
-    sha256 "47d09dd92be7d431e444c7413c51f4e650f6bc0607884d0010a44e55bc848431"
+    url "https://github.com/kaytu-io/kaytu/releases/download/v0.3.54/kaytu_0.3.54_linux_amd64.tar.gz"
+    sha256 "a03e945db5922952c955a2a5c148c83a4460ebafa0e7d7a99f45c62d232cce20"
     def install
       bin.install "kaytu" => "kaytu"
     end
